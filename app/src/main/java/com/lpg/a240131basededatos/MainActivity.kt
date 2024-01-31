77880902d4466f9074f05159f0ab30b8912cfb9b
pepilloelgrillo19 <157523378+pepilloelgrillo19@users.noreply.github.com>
2024-01-31 17:21:18 +0100

    Versión recien creada

---
+++ app/src/main/java/com/lpg/a240131basededatos/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.lpg.a240131basededatos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
