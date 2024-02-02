4545ff2d1c64e5b70c682b65f485a6b5210cb6c8
pepilloelgrillo19 <157523378+pepilloelgrillo19@users.noreply.github.com>
2024-02-02 15:39:38 +0100

    Version con introducción y salida en bruto, y consulta individual por ID

--- app/src/main/java/com/lpg/a240131basededatos/MainActivity.kt
+++ app/src/main/java/com/lpg/a240131basededatos/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.lpg.a240131basededatos
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.UserHandle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -21,6 +19,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var querFull: TextView
     private lateinit var queName: TextView
     private lateinit var querEmail: TextView
+    private lateinit var partQuerBut : Button
+    private lateinit var querID : EditText
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,9 +30,12 @@ class MainActivity : AppCompatActivity() {
         namUser = findViewById(R.id.nombreUser)
         emUser = findViewById(R.id.emailUser)
         saveBut = findViewById(R.id.saveButton)
-        querBut = findViewById(R.id.queryButton)
+        querBut = findViewById(R.id.fullQueryButton)
         querFull = findViewById(R.id.querCompleta)
         queName = findViewById(R.id.recupName)
+        querEmail = findViewById(R.id.recupEmail)
+        partQuerBut = findViewById(R.id.recupButton)
+        querID = findViewById(R.id.pedirId)
 
         db = DatabaseHandler(this)
 
@@ -60,8 +63,6 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, "Te falta algún campo por rellenar", Toast.LENGTH_LONG).show()
 
             }
-
-
         }
         querBut.setOnClickListener {
             val contactList = db.recorrerBBDD()
@@ -71,9 +72,44 @@ class MainActivity : AppCompatActivity() {
                 Log.d("Contacto ", "ID: ${contact.id}, Nombre: ${contact.name}, Email: ${contact.email}")
 
             }
-
             querFull.text = ""
             querFull.text = contactList.joinToString()
+        }
+        partQuerBut.setOnClickListener {
+            queName.text = ""
+            querEmail.text = ""
+            val querId2 = querID.text.toString().toIntOrNull()
+
+            val contactList = db.recorrerBBDD()
+            for (contact in contactList){
+
+                    if(contact.id == querId2){
+                        queName.text= contact.name
+                        querEmail.text = contact.email
+                    }
+
+            }
+
+
+
+
+
+            /* if (querId == null){
+                Toast.makeText(applicationContext, "Introduce una ID válida", Toast.LENGTH_LONG).show()
+            }else{
+                val contactList = db.recorrerBBDD()
+                for (contact in contactList){
+
+                    if(contact.id == querId){
+                        queName.text= contact.name
+                        querEmail.text = contact.email
+                    }
+
+                }
+
+
+            }*/
+
 
 
         }
