74628408ebc697b84dd8855bad4364b7cfd5773f
pepilloelgrillo19 <157523378+pepilloelgrillo19@users.noreply.github.com>
2024-02-07 18:40:20 +0100

    Aplicación funcionando con el registro, login y recuperación basada en FireBase

--- app/src/main/java/com/lpg/a240131basededatos/Registrar.kt
+++ app/src/main/java/com/lpg/a240131basededatos/Registrar.kt
@@ -1,10 +1,15 @@
 package com.lpg.a240131basededatos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
@@ -32,6 +37,50 @@ class Registrar : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
 
         dbReference = database.reference.child("User")
+    }
+    //Es una función pública, que se activa al presionar el botón (desde fuera)
+    fun registrar(view: View){
+        createNewAccount()
+    }
+    //Función privada que solo puede llamarse desde el código
+    private fun createNewAccount(){
+        val name:String = usNameReg.text.toString()
+        val lastName:String = usSurnReg.text.toString()
+        val email:String = usEmailReg.text.toString()
+        val password:String = usPasswReg.text.toString()
 
+        //Comprueba si los campos están vacios o no
+        if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(lastName) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+            progBar2.visibility = View.VISIBLE
+
+            //Esta función viene de Firebase, y es para realizar el registro
+            auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this){
+                task->
+                if(task.isComplete){
+                    //Variable de clase FireBase para generar la autenficación
+                    val user: FirebaseUser?=auth.currentUser
+                    verifyEmail(user)
+                    val userBD = dbReference.child(user!!.uid)
+                    userBD.child("Name").setValue(name)
+                    userBD.child("Apellido").setValue(lastName)
+                    //Lanzamos la actividad de Login
+                    action()
+                }
+            }
+        }
+    }
+    private fun action(){
+        startActivity(Intent(this,Login::class.java))
+    }
+    private fun verifyEmail(user:FirebaseUser?){
+
+        user?.sendEmailVerification()
+            ?.addOnCompleteListener(this){
+                task->
+                if(task.isComplete) { Toast.makeText(this, "Email enviado",Toast.LENGTH_SHORT).show()
+                } else{
+                    Toast.makeText(this, "Error al enviar el email",Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/lpg/a240131basededatos/Remind.kt
+++ app/src/main/java/com/lpg/a240131basededatos/Remind.kt
@@ -1,11 +1,43 @@
 package com.lpg.a240131basededatos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class Remind : AppCompatActivity() {
+
+    private lateinit var usEmailRem: EditText
+    private lateinit var progBar3: ProgressBar
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_remind_layout)
+
+        usEmailRem=findViewById(R.id.userEmailRemind)
+        progBar3=findViewById(R.id.progressBar3)
+
+        auth = FirebaseAuth.getInstance()
     }
+fun remind(view:View){
+    val email=usEmailRem.text.toString()
+    if(!TextUtils.isEmpty(email))
+        progBar3.visibility = View.VISIBLE
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(this) {
+            task ->
+            if(task.isSuccessful){
+                startActivity(Intent(this,Login::class.java))
+            } else {
+                Toast.makeText(this, "Error al enviar el Correo de recuperación", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
 }
