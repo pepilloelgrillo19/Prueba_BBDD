5afe6d0a206e75faadd236345e988401378a7a8f
pepilloelgrillo19 <157523378+pepilloelgrillo19@users.noreply.github.com>
2024-02-07 18:40:20 +0100

    Aplicación funcionando con el registro, login y recuperación basada en FireBase

--- app/src/main/java/com/lpg/a240131basededatos/Login.kt
+++ app/src/main/java/com/lpg/a240131basededatos/Login.kt
@@ -1,9 +1,13 @@
 package com.lpg.a240131basededatos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
@@ -23,8 +27,39 @@ class Login : AppCompatActivity() {
         progBar=findViewById(R.id.progressBar)
 
         auth= FirebaseAuth.getInstance()
+    }
 
+    fun remind(view: View) {
+        startActivity(Intent(this,Remind::class.java))
+    }
 
+    fun registrar(view:View) {
+        startActivity(Intent(this,Registrar::class.java))
+    }
 
+    fun login(view:View) {
+        loginUser()
+    }
+
+    private fun loginUser(){
+        val user:String = usLogNam.text.toString()
+        val password:String = usLogPass.text.toString()
+        if( !TextUtils.isEmpty(user)
+            && !TextUtils.isEmpty(password) )
+        {
+            progBar.visibility = View.VISIBLE
+            auth.signInWithEmailAndPassword(user,password).addOnCompleteListener(this){
+                task->
+                if (task.isSuccessful) {
+                    action()
+                }else{
+                    Toast.makeText(this,"Error en la autenticación",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun action(){
+        startActivity(Intent(this,MainActivity::class.java))
     }
 }
